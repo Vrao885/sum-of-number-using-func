a4626dcfced1996c83272bebeb758c02a39c5ee5
Vaishali Rao <116837558+Vrao885@users.noreply.github.com>
2023-07-06 23:27:29 +0530

    Add files via upload

---
+++ sum_func.py
@@ -0,0 +1,10 @@
+def sum (list): 
+    count= 0     
+    for i in list: 
+        
+        count=count+i 
+    return count
+list1 = [1, 2, 3, 4]
+    
+result = sum(list1)   
+print("The Sum of the List is:", result)
